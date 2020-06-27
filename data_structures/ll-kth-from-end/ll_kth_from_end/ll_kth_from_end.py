7178ceb46745b1c02e8fcb685163777931e7b519
Richard Whitehead <damian253@me.com>
2020-06-27 08:38:06 -0700

    updates

--- data_structures/ll-kth-from-end/ll_kth_from_end/ll_kth_from_end.py
+++ data_structures/ll-kth-from-end/ll_kth_from_end/ll_kth_from_end.py
@@ -1,18 +1,68 @@
-def kth_from_end(self, k):
+class Node:
+    """ Class for the Node instances"""
 
-    length = self.length_()
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    if not -length <= k < length:
-        raise Exception("k not in the range")
+class LinkedList:
+    """ Class for the LinkedLists instances"""
 
-    step_count = None
+    def __init__(self):
+        """method to initiate a LinkedList"""
 
-    if k >= 0:
+        self.head = None
+
+    def __repr__(self):
+        """method to represent that LinkedList created"""
+
+        return "LinkedList created"
+
+    def insert(self, value):
+        """method to insert new node to the beginning of the list"""
+
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+
+    def includes(self, value):
+        """method to check if the given value in the liked list"""
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def __str__(self):
+        """method that returns a string that represents all list elements"""
+
+        list_str = ''
+        current = self.head
+        while current:
+            # print(current, "current")
+            list_str += str(current.value ) + ', '
+            current = current.next
+        
+        return list_str[:-2]
+    
+    def kth_from_end(self, k):
+
+        length = self.length_()
+
+        if not -length <= k < length:
+            raise Exception("k not in the range")
+
+        step_count = None
+
+        if k >= 0:
             step_count = length - k -1
-    if k < 0:
+        if k < 0:
             step_count = abs(k)-1
 
-    current = self.head
-    for _ in range(step_count):
-        current = current.next
+        current = self.head
+        for _ in range(step_count):
+            current = current.next
+            
         return current.value
--- data_structures/ll-kth-from-end/tests/test_ll_kth_from_end.py
+++ data_structures/ll-kth-from-end/tests/test_ll_kth_from_end.py
@@ -1,5 +1,20 @@
-from ll_kth_from_end import __version__
+import pytest
 
+from linked_list import Node, LinkedList
 
-def test_version():
-    assert __version__ == '0.1.0'
+linked_list = LinkedList()
+values = ['a', 'b', 'c', 'd','e']
+
+for el in values:
+    linked_list.insert(el)
+
+def test_list_nodes_order():
+    assert "e, d, c, b, a" == linked_list.__str__()
+
+def test_list_1_elem():
+    linked_list = LinkedList()
+    linked_list.insert('z')
+    assert linked_list.kth_from_end(0) == 'z'
+    assert linked_list.kth_from_end(-1) == 'z'
+    with pytest.raises(Exception):  # Pass in the expected error
+        linked_list.kth_from_end(2)
