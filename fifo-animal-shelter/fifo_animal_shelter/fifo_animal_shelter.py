5cbbfff3c1323518c081c46ec7262ebd863cc941
Richard Whitehead <58717816+RichWhitehead@users.noreply.github.com>
2020-07-06 22:01:57 -0700

    Update fifo_animal_shelter.py

--- fifo-animal-shelter/fifo_animal_shelter/fifo_animal_shelter.py
+++ fifo-animal-shelter/fifo_animal_shelter/fifo_animal_shelter.py
@@ -80,27 +80,5 @@ class AnimalShelter:
         return adopted_pet
  
 
-class PseudoQueue:
-  def __init__(stack, s1):
-    self.s1 = s1
-    self.s2 = Stack()
-    
-def enqueue(self, value):
-  self.s1.push(value)
-  
-def dequeue(self):
-  
-  if self.s1.top == None:
-    raise Exception('empty PseudoQueue')
-  while self.s1.top != None:
-    pope_file = self.s1.pop()
-    
-    self.s2 = self.s1.pop(poped_file)
-    
-    pop_val = self.s2.pop()
-    
-    while self.s2.top != None:
-      self.s1.push(self.s2.pop())
-      
-      return pop_val
+
         
