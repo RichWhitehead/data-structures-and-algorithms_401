bdcdce45dfda9ee572546a2ec2b9d9761145d5af
Richard Whitehead <damian253@me.com>
2020-07-26 20:55:27 -0700

    CC15 complete

--- tree/tree.py
+++ tree/tree.py
@@ -13,11 +13,14 @@ class BinaryTree(object):
   def print_tree(self, traversal_type):
     if traversal_type == "preOrder":
       return self.preOrder(tree.root)
-    
+    elif traversal_type == "inOrder":
+      return self.inOrder(tree.root)
+    elif traversal_type == "postOrder":
+      return self.postOrder(tree.root)
     else:
       print("Traversal type" + str(traversal_type) + "is not supported.")
     
-# This method will preorder root --> Left --> Right
+# This method will preOrder: Root --> Left --> Right 12453678
 
   def preOrder(self, start):
     traversal = ""
@@ -25,8 +28,29 @@ class BinaryTree(object):
         traversal += (str(start.value) + "") 
         traversal += self.preOrder(start.left)
         traversal += self.preOrder(start.right)
-        # print(traversal)
+       
     return traversal
+# This method will inOrder: Left--> Root --> Right  42516378
+  def inOrder(self, start):
+    traversal = ""
+    if start:
+        traversal += self.inOrder(start.left)
+        traversal += (str(start.value) + "") 
+        traversal += self.inOrder(start.right)
+    return traversal
+  
+# This method will postOrder: Left --> Right --> Root   45268731
+  def postOrder(self, start):
+    traversal = ""
+    if start:
+        traversal += self.postOrder(start.left)
+        traversal += self.postOrder(start.right)
+        traversal += (str(start.value) + "") 
+    return traversal
+        
+    
+  
+  
       
 tree = BinaryTree(1)
 tree.root.left = Node(2)
@@ -38,6 +62,10 @@ tree.root.right.right = Node(7)
 tree.root.right.right.right = Node(8)
 
 print(tree.print_tree("preOrder"))
+print(tree.print_tree("inOrder"))
+print(tree.print_tree("postOrder"))
+
+
 
 
       
