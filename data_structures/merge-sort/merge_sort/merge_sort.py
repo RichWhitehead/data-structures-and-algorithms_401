34bd8cdec3a6a95322c32c35534d036fcd17c011
Richard Whitehead <damian253@me.com>
2020-08-25 14:27:42 -0700

    almost complete

--- data_structures/merge-sort/merge_sort/merge_sort.py
+++ data_structures/merge-sort/merge_sort/merge_sort.py
@@ -1,22 +1,43 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr): 
-    if len(arr) >1: 
-        mid = len(arr)//2 # Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
-  
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
-  
+
+def merge_sort(arr):
+    
+    if len(arr) >1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
         i = j = k = 0
-          
-        # Copy data to temp arrays L[] and R[] 
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
                 i+= 1
-            else: 
-                arr[k] = R[j] 
+            else:
+                arr[k] = R[j]
                 j+= 1
-            k+= 1
+                k+= 1
+                while i < len(L):
+                    arr[k] = L[i]
+                    i+= 1
+                    k+= 1
+                    
+                    while j < len(R):
+                        arr[k] = R[j]
+                        j+= 1
+                        k+= 1
+def printList(arr):
+    
+    for i in range(len(arr)):
+        print(arr[i], end =" ")
+        print()
+        
+if __name__ == '__main__':
+    
+    arr = [8,4,23,42,16,15]
+print ("Given array is")
+printList(arr)
+merge_sort(arr)
+print("Sorted array is: ")
+printList(arr) 
+
           
